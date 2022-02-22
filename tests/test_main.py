70c36bc6e919bb19e95d00fb6e1b46b6381a58db
Aldrick Castro <aldrickdev@gmail.com>
2022-02-22 10:22:51 -0500

    Minor update to tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,11 +9,10 @@ client = TestClient(app)
 def test_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Endpoints": ["/api/products"]}
+    assert response.json() == {"Hello": "World"}
 
 
 def test_products_endpoint():
     response = client.get("/api/products")
     assert response.status_code == 200
     assert response.json() == {"Products": products.product_list}
-    assert response.json() != {"Endpoints": ["/api/products"]}
